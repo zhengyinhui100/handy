0975682ee5c0699df6afaa59f829f1cfc90a9c1c
zyh <zhengyinhui100@gmail.com>
2013-12-19 14:34:47 +0800

    添加hash方法

--- src/base/util.js
+++ src/base/util.js
@@ -5,12 +5,23 @@
 handy.add('Util',function($){
 	
 	var Util={
-		isWindow         : fIsWindow, //检查是否是window对象
-		getUuid          : fGetUuid   //获取handy内部uuid
+		isWindow         : fIsWindow,  //检查是否是window对象
+		getUuid          : fGetUuid,   //获取handy内部uuid
+		getHash          : fGetHash,   //获取hash，不包括“？”开头的query部分
+		setHash          : fSetHash    //设置hash，不改变“？”开头的query部分
 	}
 	
 	var _nUuid=0;
 	
+	/**
+	 * 检查是否是window对象
+	 * @method  isWindow
+	 * @param {*}obj 参数对象
+	 * @return  {boolean}
+	 */
+	function fIsWindow( obj ) {
+		return obj != null && obj == obj.window;
+	}
 	/**
 	 * 获取handy内部uuid
 	 * @method  getUuid
@@ -20,13 +31,22 @@ handy.add('Util',function($){
 		return _nUuid++;
 	}
 	/**
-	 * 检查是否是window对象
-	 * @method  isWindow
-	 * @param {*}obj 参数对象
-	 * @return  {boolean}
+	 * 获取hash，不包括“？”开头的query部分
+	 * @method getHash
+	 * @return {?string} 返回hash
 	 */
-	function fIsWindow( obj ) {
-		return obj != null && obj == obj.window;
+	function fGetHash(){
+		var sHash=top.location.hash;
+		return sHash.replace(/\?.*/,'');
+	}
+	/**
+	 * 设置hash，不改变“？”开头的query部分
+	 * @method setHash
+	 * @param {string} sHash要设置的hash
+	 */
+	function fSetHash(sHash){
+		var sHash=top.location.hash;
+		return sHash.replace(/#[^\?]*/,sHash);
 	}
 	
 	return Util;
