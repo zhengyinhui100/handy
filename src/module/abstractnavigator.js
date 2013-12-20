023d604a92523dfdc1670fb8adf1ea4f088fc0e7
zyh <zhengyinhui100@gmail.com>
2013-12-20 12:21:17 +0800

    导航基类改名

--- src/module/abstractnavigator.js
+++ src/module/abstractnavigator.js
@@ -13,7 +13,7 @@ $Define("handy.module.Navigator","handy.base.Object",function (Object) {
 	var Navigator = Object.createClass();
 	
 	Object.extend(Navigator.prototype, {
-		navigate      : function(){}      //显示导航效果，参数是模块名
+		navigate      : function(){}      //显示导航效果，参数是当前进入的模块实例和模块管理类实例，此方法返回true表示不需要模块管理类的导航功能
 	});
 	
 	return Navigator;
