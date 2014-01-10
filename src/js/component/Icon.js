957d742f3ded3a06fe6bbc62e736a6e2b975d50c
zyh <zhengyinhui100@gmail.com>
2014-01-10 12:54:46 +0800

    handy目录更改，去掉jQuery Mobile

---
+++ src/js/component/Icon.js
@@ -0,0 +1,23 @@
+/**
+ * 图标类
+ * @author 郑银辉(zhengyinhui100@gmail.com)
+ * @created 2014-01-01
+ */
+
+$Define('handy.component.Icon','handy.component.AbstractComponent',function(AbstractComponent){
+	
+	
+	var Obj=$H.Object;
+	var Icon=Obj.createClass();
+	
+	Obj.inherit(Icon,AbstractComponent);
+	
+	Obj.extend(Icon,{
+	});
+	
+	Obj.extend(Icon.prototype,{
+	});
+	
+	return Icon;
+	
+});
