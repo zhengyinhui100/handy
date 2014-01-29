b9324ceb99d3524f75de62fcd0c222b5f08150fd
zyh <zhengyinhui100@gmail.com>
2014-01-29 10:20:49 +0800

    增加新组件文件

---
+++ src/js/component/Radio.js
@@ -0,0 +1,30 @@
+/**
+ * 图标类
+ * @author 郑银辉(zhengyinhui100@gmail.com)
+ * @created 2014-01-29
+ */
+
+$Define('c.Radio',
+'c.AbstractComponent',
+function(AC){
+	
+	var Radio=AC.define('Radio');
+	//检查浏览器是否支持svg，不支持则添加标记class
+	$H.Support.testSvg(function(bSupport){
+		if(!bSupport){
+			$('html').addClass('hui-nosvg');
+		}
+	})
+	
+	$HO.extend(Radio.prototype,{
+		//初始配置
+		hasBg           : true,               //是否有背景
+//		name            : '',                  //图标名称
+		
+		tmpl            : ['<span class="hui-icon hui-icon-<%=this.name%><%if(this.hasBg){%> hui-icon-bg<%}%>"></span>']
+		
+	});
+	
+	return Radio;
+	
+});
