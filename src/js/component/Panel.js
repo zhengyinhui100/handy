238cca232ab27e8b50c4f9dabad07d668b6526c9
zyh <zhengyinhui100@gmail.com>
2014-03-22 11:58:16 +0800

    添加Panel类

---
+++ src/js/component/Panel.js
@@ -0,0 +1,25 @@
+/**
+ * 面板类
+ * @author 郑银辉(zhengyinhui100@gmail.com)
+ * @created 2014-01-01
+ */
+
+$Define('C.Panel',
+'C.AbstractComponent',
+function(AC){
+	
+	var Panel=AC.define('Panel');
+	
+	Panel.extend({
+		//初始配置
+//		content         : '',                 //内容
+		
+		tmpl            : [
+			'<div><%=this.content||this.getHtml(">*")%><div>'
+		]
+		
+	});
+	
+	return Panel;
+	
+});
