13500b78e564b4a44c5163e4f61c1f015e22cadb
zyh <zhengyinhui100@gmail.com>
2014-03-29 01:17:04 +0800

    cache相关修改

--- src/js/module/AbstractModule.js
+++ src/js/module/AbstractModule.js
@@ -18,17 +18,26 @@ $Define("M.AbstractModule","CM.View",function (View) {
 //		isActived      : false,          //{boolean}模块是否是当前活跃的
 //		renderTo       : null,           //自定义模块容器，{jQuery}对象或选择器
 		                                 //模块初始化后以_container为准，获取需用getEl方法
-		useCache       : true,           //{boolean}是否使用cache
+//		notCache       : false,          //{boolean}是否不使用cache，默认使用,仅当配置成true时不使用
+//      clearCache     : false,          //仅清除一次当前的缓存，下次进入模块时执行清除并恢复原先缓存设置
 //		name           : null,           //{string}模块名
 //		chName         : null,           //{string}模块的中文名称
 		
 //		getData        : null,           //{function()}获取该模块的初始化数据
 //		clone          : null,           //{function()}克隆接口
-		cache          : function(){},   //显示模块缓存
-		init           : function(){},   //初始化函数, 在模块创建后调用（在所有模块动作之前）
-		reset          : function(){},   //重置函数, 在该模块里进入该模块时调用
-		exit           : function(){return true}   //离开该模块前调用, 返回true允许离开, 否则不允许离开
+		useCache       : $H.noop,        //判断是否使用模块缓存
+		cache          : $H.noop,        //显示模块缓存时调用
+		init           : $H.noop,        //初始化函数, 在模块创建后调用（在所有模块动作之前）
+		reset          : $H.noop,        //重置函数, 在该模块里进入该模块时调用
+		exit           : function(){return true},  //离开该模块前调用, 返回true允许离开, 否则不允许离开
+		cleanCache     : fCleanCache     //清除模块缓存
 	});
+	/**
+	 * 清除模块缓存
+	 */
+	function fCleanCache(){
+		this.clearCache=true;
+	}
 	
 	return AbstractModule;
 });
