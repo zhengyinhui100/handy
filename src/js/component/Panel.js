ca7d9c6daf8b298c369f7e6b1a5d209e3f6bcdba
zyh <zhengyinhui100@gmail.com>
2014-05-10 16:48:32 +0800

    Panel的loading效果

--- src/js/component/Panel.js
+++ src/js/component/Panel.js
@@ -12,12 +12,27 @@ function(AC){
 	
 	Panel.extend({
 		//初始配置
-//		content         : '',                 //内容
+//		content         : '',               //内容
+		isLoading       : false,            //是否显示正在加载中
+		loadingTxt      : '正在加载中...',   //正在加载中的提示文字
 		
 		
 		tmpl            : [
-			'<div><%=this.content||this.findHtml(">*")%></div>'
-		]
+			'<div>',
+				'<%if(this.isLoading){%>',
+					'<div class="hui-panel-loading">',
+						'<div class="hui-mask"></div>',
+						'<div class="hui-loading-container">',
+							'<div class="hui-tips hui-tips-big hui-tips-black hui-radius-little">',
+								'<span class="hui-icon hui-icon-loading"></span>',
+								'<span class="hui-tips-txt"><%=this.loadingTxt%></span>',
+							'</div>',
+						'</div>',
+					'</div>',
+				'<%}%>',
+				'<%=this.content||this.findHtml(">*")%>',
+			'</div>'
+		].join('')
 	});
 	
 	return Panel;
