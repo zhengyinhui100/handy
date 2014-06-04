0a3167f71db60267a7edc14603045a40d36c2664
zyh <zhengyinhui100@gmail.com>
2014-06-04 14:57:10 +0800

    嵌套属性bug

--- src/js/common/Model.js
+++ src/js/common/Model.js
@@ -1,5 +1,7 @@
 /**
  * 模型类，负责数据封装，可监听事件：invalid、sync、destroy、change:attr、change
+ * PS：嵌套属性区别于普通属性，不可通过hasChanged、changedAttrbutes等方法获取改变，
+ * 只能通过相关委托事件(_onAttrEvent方法里)进行监测
  * @author 郑银辉(zhengyinhui100@gmail.com)
  * @created 2014-03-06
  */
@@ -144,7 +146,7 @@ function(AbstractDao,AbstractEvents){
 						type=$H.ns(type);
 					}
 				}
-				if($H.isClass(type)&&!(val instanceof type)){
+				if($H.isClass(type)&&!(val instanceof type)&&!me.get(key)){
 					val=new type(val);
 					//监听所有事件
 					val.on('all',$H.bind(me._onAttrEvent,me,key));
@@ -306,19 +308,28 @@ function(AbstractDao,AbstractEvents){
 	    //循环进行设置、更新、删除
 	    for (var sAttr in oAttrs) {
 	   	    val = oAttrs[sAttr];
-	   	    //与当前值不相等，放入改变列表中
-	    	if (!$H.equals(oCurrent[sAttr], val)){
-	    		oChanges[sAttr]=val;
-	    	}
-	    	//与初始值不相等，放入已经改变的hash对象中
-	    	if (!$H.equals(oPrev[sAttr], val)) {
-	            me._changed[sAttr] = val;
-	    	} else {
-	    		//跟初始值相等，即没有变化
-	        	delete me._changed[sAttr];
-	    	}
+	    	var curVal=oCurrent[sAttr];
+	    	//是否是嵌套属性
+	   	    var bNested=curVal&&$H.isInstance(curVal);
 	    	//如果取消设置，删除对应属性
-	    	bUnset ? delete oCurrent[sAttr] : oCurrent[sAttr] = val;
+	    	if(bNested){
+	    		//PS:嵌套属性须调用该属性类的方法进行设置，也不需要在此触发相关事件，而是通过监听该属性上的事件触发事件
+	    		bUnset ? curVal.reset() : curVal.set(val);
+	    	}else{
+		   	    //与当前值不相等，放入本次改变列表中
+		    	if (!$H.equals(oCurrent[sAttr], val)){
+		    		oChanges[sAttr]=val;
+		    	}
+		    	//与初始值不相等，放入已经改变的hash对象中
+		    	if (!$H.equals(oPrev[sAttr], val)) {
+		            me._changed[sAttr] = val;
+		    	} else {
+		    		//跟初始值相等，即没有变化
+		        	delete me._changed[sAttr];
+		    	}
+		    	
+		    	bUnset ? delete oCurrent[sAttr] : oCurrent[sAttr] = val;
+	    	}
 	    }
 	    
 		var bHasChange=!$H.isEmpty(oChanges);
