e751150f9edddc7eff429787fa85e193630aa009
zyh <zhengyinhui100@gmail.com>
2014-07-11 13:28:41 +0800

    构建脚本

--- tools/Gruntfile.js
+++ tools/Gruntfile.js
@@ -2,18 +2,26 @@ module.exports = function(grunt) {
 	'use strict';
 
 	var projectDir='/Users/hui/Documents/workspace/sportapp/';
+	var deployDir='/Users/hui/Documents/';
+	
 	var buildDir=projectDir+'build/';
 	var wwwDir=projectDir+'www/'
 	var cssDir=wwwDir+'css/';
 	var buildVersionDir=buildDir;
 	var buildLibDir=buildVersionDir+'lib/';
-	var deployDir='~/Documents/';
+	var appDir=deployDir+'appBuild/';
+	var testConfig={
+		serverName:'http://115.28.151.237:8080/',
+		staticServerName:'http://115.28.151.237/',
+		environment:'test'
+	}
 	
 	grunt.initConfig({
 		pkg : grunt.file.readJSON('package.json'),
 		clean:{
 			options:{force:true},
-			src:[cssDir,buildDir]
+			build:[cssDir,buildDir],
+			appBuild:[appDir]
 		},
 		less : {
 			src : {
@@ -25,11 +33,29 @@ module.exports = function(grunt) {
 			}
 		},
 		copy:{
-			all:{
+			options : {
+				//二进制文件必须排除，否则会损坏文件
+				noProcess:['**/*.jpg','**/*.png','**/*.jpeg','**/*.gif','**/*.swf'],
+				process : function(content, srcpath) {
+					if(/\.(js|html)$/.test(srcpath)){
+						return content.replace(/{%=(((?!%}).)+)%}/g, function(match,$1){
+							return testConfig[$1];
+						});
+					}
+					return content;
+				}
+			},
+			build:{
 				expand : true,
 				cwd:wwwDir,
-				src : ['css/**','js/**','img/**','lib/**'],
+				src : ['css/**/*','img/**/*','js/**/*','lib/**/*'],
 				dest :buildVersionDir
+			},
+			appBuild:{
+				expand : true,
+				cwd:projectDir,
+				src : ['**/*','!platforms/**/*','!plugins/**','!build/**','!htmlTmpl/**','!less/**',/*'!www/img/**',*/'!www/js/**','!www/lib/**'],
+				dest :appDir
 			}
 		},
 		handy_require:{
@@ -139,14 +165,35 @@ module.exports = function(grunt) {
 			}
 		},
 		shell : {
-			deploy : {
+			build : {
 				command : [
 					'cp -R ../build '+deployDir+'handy',
 					'cp -R '+buildVersionDir+' '+deployDir+'<%=pkg.name%>',
 					'cd '+deployDir,
-					'tar -cvzf app.tar.gz <%=pkg.name%> handy',
+					'tar -cvzf static.tar.gz <%=pkg.name%> handy',
 					'rm -r handy',
 					'rm -r <%=pkg.name%>'
+				].join('&&')
+			},
+			appTar:{
+				command : [
+					'cd '+appDir,
+					'zip -r '+deployDir+'app.zip www'
+				].join('&&')
+			},
+			buildDevApk:{
+				command : [
+					'cd '+projectDir,
+					'rm -f plugins/android.json',
+					'rm -fR platforms/android',
+					'phonegap local build android'
+				].join('&&')
+			},
+			buildTestApk:{
+				command : [
+					'cd '+appDir,
+					'cp -R '+projectDir+'.cordova .cordova',
+					'phonegap local build android'
 				].join('&&')
 			}
 		}
@@ -166,7 +213,17 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-tar.gz');
 	grunt.loadNpmTasks('grunt-shell');
 
-	grunt.registerTask('online', ['clean','less','copy','handy_require','concat','cssmin','uglify','shell']);
-	grunt.registerTask('default', ['shell']);
+	
+	grunt.registerTask('test', ['clean:build','less','copy:build','handy_require','concat','cssmin','uglify','shell:build']);
+	
+	grunt.registerTask('appBuild', ['clean:appBuild','copy:appBuild']);
+	
+	grunt.registerTask('appTar', ['appBuild','shell:appTar']);
+	
+	grunt.registerTask('bulidTestApk', ['appBuild','shell:buildTestApk']);
+	
+	grunt.registerTask('bulidDevApk', ['shell:buildDevApk']);
+	
+	grunt.registerTask('default', ['appTar']);
 	
 };
