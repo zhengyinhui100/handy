cb1e37986f4c58f6516e8e78fc5d26915e2f8c13
zyh <zhengyinhui100@gmail.com>
2014-07-18 00:55:50 +0800

    添加检测表单是否改变的方法

--- src/js/component/Form.js
+++ src/js/component/Form.js
@@ -23,9 +23,50 @@ function(AC){
 					'{{placeItem}}',
 				'</form>',
 			'</div>'
-		].join('')
+		].join(''),
+		
+		hasChanged      : fHasChanged      //检测是否有表单改变的域/值
 		
 	});
+	
+	/**
+	 * 检测是否有表单改变的域/值
+	 * @return {boolean} true表示有变化
+	 */
+	function fHasChanged(){
+		var me=this;
+		var bHasChange=false;
+		me.findEl('input,textarea').each(function(i,oEl){
+			if(oEl.type==='checkbox'||oEl.type==='radio'){
+				if (oEl.checked != oEl.defaultChecked){
+					bHasChange=true;
+					return false;
+				}
+			}else{
+				if (oEl.value != oEl.defaultValue){
+					bHasChange=true;
+					return false;
+				}
+			}
+		});
+		me.findEl('select').each(function(i,oEl){
+			var def = 0, opt;
+			for (var i = 0, len = oEl.options.length; i < len; i++) {
+				opt = oEl.options[i];
+				bHasChange = bHasChange || (opt.selected != opt.defaultSelected);
+				if (opt.defaultSelected){
+					def = i;
+				}
+			}
+			if (bHasChange && !oEl.multiple){
+				bHasChange = (def != oEl.selectedIndex);
+			}
+			if (bHasChange){
+				return false;
+			}
+		});
+		return bHasChange;
+	}
 	
 	return Form;
 	
